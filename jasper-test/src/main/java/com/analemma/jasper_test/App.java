8471b743fe07dff7943cea934349edd8aad447e6
mladenc <mladenc@localhost>
2014-01-28 16:52:48 +0100

    initial import

---
+++ jasper-test/src/main/java/com/analemma/jasper_test/App.java
@@ -0,0 +1,45 @@
+package com.analemma.jasper_test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JFrame;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.swing.JRViewer;
+
+
+public class App {
+	public static void main(String[] args) {
+		String sourceFileName = "/home/mladenc/Workspaces/default/jasper-test/src/main/resources/Test_A4.jrxml";
+
+		System.out.println("Compiling Report Design ...");
+		try {
+			String compileeFileName = JasperCompileManager
+					.compileReportToFile(sourceFileName);
+
+			DataBeanList DataBeanList = new DataBeanList();
+			ArrayList<DataBean> dataList = DataBeanList.getDataBeanList();
+
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(
+					dataList);
+
+			JFrame frame = new JFrame("Report");
+			JasperPrint jasperPrint = JasperFillManager.fillReport(
+					compileeFileName, parameters, beanColDataSource);
+			frame.getContentPane().add(new JRViewer(jasperPrint));
+			frame.pack();
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Done compiling!!! ...");
+	}
+}
---
+++ jasper-test/src/main/java/com/analemma/jasper_test/DataBean.java
@@ -0,0 +1,22 @@
+package com.analemma.jasper_test;
+
+public class DataBean {
+	private String name;
+	private String country;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
